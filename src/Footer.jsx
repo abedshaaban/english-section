9f2f42783e4f43f0f52d58f06717d96b6a72e8ed
lilXA1 <abedshaaban600@gmail.com>
2022-04-09 23:14:47 +0300

    added resourses link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,12 +5,20 @@ function Footer() {
     <div className="footer">
       <section className="footer_S">
         <section className="S_community">
+          <span>Resources:</span>
+          <a href="https://student-affair.netlify.app/#/12" target="_blank">go to resources</a>
+        </section>
+
+        <section className="S_community">
           <span>Community:</span>
           <span>contact us</span>
           <span>terms & conditions</span>
           <span>privacy</span>
         </section>
-        <section className="S_community">done by: abed al ghani shaaban</section>
+
+        <section className="S_community">
+          done by: abed al ghani shaaban
+        </section>
       </section>
     </div>
   );
