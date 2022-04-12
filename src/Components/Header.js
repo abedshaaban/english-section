ead1b229125dc61c04cdab1105897d47f60c3a99
lilXA1 <abedshaaban600@gmail.com>
2022-04-12 22:23:33 +0300

    .

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,22 @@ import "./Styles/Header.css";
 function Header() {
   return (
     <section className="header">
-      <a href="https://student-affair.netlify.app/#/12" target="_blank" rel="noreferrer">
-        <Button variant="outlined">go to resources</Button>
+      <a
+        href="https://student-affair.netlify.app/#/12"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Button
+          variant="outlined"
+          sx={{
+            color: "#9c27b0",
+            borderColor: "#9c27b0",
+            fontSize: "15px",
+            fontWeight: "800",
+          }}
+        >
+          go to resources
+        </Button>
       </a>
     </section>
   );
